677c53747445fde82910378b67b0e3f4b249f57c
bchiavarini <b.chiavarini@cineca.it>
2021-02-17 18:46:55 +0100

    restored endpoint family

--- projects/nig/backend/endpoints/family.py
+++ projects/nig/backend/endpoints/family.py
@@ -1,13 +1,10 @@
-from typing import Optional
-
 from nig.endpoints import PHENOTYPE_NOT_FOUND, NIGEndpoint
 from restapi import decorators
 from restapi.connectors import neo4j
 from restapi.exceptions import BadRequest, NotFound
 from restapi.models import Schema, fields, validate
 from restapi.rest.definition import Response
-
-RELATIONSHIP = ["father", "mother"]
+from restapi.utilities.logs import log
 
 
 class Family(NIGEndpoint):
@@ -20,41 +17,38 @@ class Family(NIGEndpoint):
         summary="Create a new relationship between two phenotypes",
         responses={
             200: "Relationship successfully created",
+            400: "Cannot set relationship between the requested phenotypes",
             404: "This phenotype cannot be found or you are not authorized to access",
         },
     )
+    @decorators.use_kwargs(
+        {
+            "relationship": fields.Str(
+                required=True, validate=validate.OneOf(["father", "mother"])
+            )
+        },
+        location="body",
+    )
     @decorators.graph_transactions
-    def post(self, uuid1, uuid2):
+    def post(self, uuid1: str, uuid2: str, relationship: str) -> Response:
 
         graph = neo4j.get_instance()
-        v = self.get_input()
-        allowed = ["father", "mother"]
-
-        if "type" not in v:
-            return self.response(
-                "Missing relationship type, allowed values are: %s" % allowed
-            )
-
-        relationship = v["type"]
 
         if uuid1 == uuid2:
             raise BadRequest(f"Cannot set relationship between {uuid1} and itself")
-
-        if relationship not in allowed:
-            raise BadRequest(f"Unknown relationship, allowed values are: {allowed}")
 
         phenotype1 = graph.Phenotype.nodes.get_or_none(uuid=uuid1)
         if phenotype1 is None:
             raise NotFound(PHENOTYPE_NOT_FOUND)
 
-        study = self.getSingleLinkedNode(phenotype1.defined_in)
+        study = phenotype1.defined_in.single()
         self.verifyStudyAccess(study, error_type="Phenotype", read=True)
 
         phenotype2 = graph.Phenotype.nodes.get_or_none(uuid=uuid2)
         if phenotype2 is None:
             raise NotFound(PHENOTYPE_NOT_FOUND)
 
-        study = self.getSingleLinkedNode(phenotype2.defined_in)
+        study = phenotype2.defined_in.single()
         self.verifyStudyAccess(study, error_type="Phenotype", read=True)
 
         if relationship == "father":
@@ -65,6 +59,11 @@ class Family(NIGEndpoint):
             phenotype2.son.connect(phenotype1)
             phenotype1.mother.connect(phenotype2)
 
+        self.log_event(
+            self.events.modify,
+            phenotype1,
+            f"set relationship {relationship} with {phenotype2.uuid}",
+        )
         return self.empty_response()
 
     @decorators.auth.require()
@@ -76,32 +75,31 @@ class Family(NIGEndpoint):
             404: "This phenotype cannot be found or you are not authorized to access",
         },
     )
+    @decorators.use_kwargs(
+        {
+            "relationship": fields.Str(
+                required=True, validate=validate.OneOf(["father", "mother", "son"])
+            )
+        },
+        location="body",
+    )
     @decorators.graph_transactions
-    def delete(self, uuid1, uuid2):
+    def delete(self, uuid1: str, uuid2: str, relationship: str) -> Response:
 
         graph = neo4j.get_instance()
-        v = self.get_input()
-        allowed = ["father", "mother"]
-
-        if "type" not in v:
-            raise BadRequest(
-                f"Missing relationship type, allowed values are: {allowed}"
-            )
-
-        relationship = v["type"]
 
         phenotype1 = graph.Phenotype.nodes.get_or_none(uuid=uuid1)
         if phenotype1 is None:
             raise NotFound(PHENOTYPE_NOT_FOUND)
 
-        study = self.getSingleLinkedNode(phenotype1.defined_in)
+        study = phenotype1.defined_in.single()
         self.verifyStudyAccess(study, error_type="Phenotype", read=True)
 
         phenotype2 = graph.Phenotype.nodes.get_or_none(uuid=uuid2)
         if phenotype2 is None:
             raise NotFound(PHENOTYPE_NOT_FOUND)
 
-        study = self.getSingleLinkedNode(phenotype2.defined_in)
+        study = phenotype2.defined_in.single()
         self.verifyStudyAccess(study, error_type="Phenotype", read=True)
 
         # [1] - FATHER -> [2]
@@ -127,4 +125,9 @@ class Family(NIGEndpoint):
             if phenotype2.father.is_connected(phenotype1):
                 phenotype2.father.disconnect(phenotype1)
 
+        self.log_event(
+            self.events.modify,
+            phenotype1,
+            f"delete relationship {relationship} with {phenotype2.uuid}",
+        )
         return self.empty_response()
