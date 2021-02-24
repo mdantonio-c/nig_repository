4c42cf1ca46705bd6e55621d1b3430b3bf33395b
Mattia <m.dantonio81@gmail.com>
2021-02-24 06:48:13 +0100

    Bump RAPyDo version to 1.1

--- projects/nig/backend/initialization.py
+++ projects/nig/backend/initialization.py
@@ -1,3 +1,7 @@
 class Initializer:
     def __init__(self, app=None):
         pass
+
+    # This method is called after normal initialization if TESTING mode is enabled
+    def initialize_testing_environment(self):
+        pass
