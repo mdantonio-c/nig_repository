5e9251ac69ca0d22219dc840a714629437098bea
Mattia <m.dantonio@cineca.it>
2021-06-24 07:29:16 +0200

    Debug code

--- projects/nig/backend/scripts/init_hpo.sh
+++ projects/nig/backend/scripts/init_hpo.sh
@@ -8,8 +8,10 @@ then
     mv ${DATA_IMPORT_FOLDER}/hp.obo ${DATA_IMPORT_FOLDER}/hp.obo.bak
 fi
 
+echo "Downloading http://purl.obolibrary.org/obo/hp.obo ..."
 wget --quiet http://purl.obolibrary.org/obo/hp.obo -O ${DATA_IMPORT_FOLDER}/hp.obo
 
+echo "hp.obo successfully downloaded, starting parser"
 cd /code/nig/scripts/
 
 time python3 parsing_hpo.py
