a436623295e496073a5ba22574864a914043d85e
Mattia D'Antonio <m.dantonio@cineca.it>
2022-01-12 09:27:31 +0100

    Compatibility fixes

--- projects/nig/backend/tasks/launch_pipeline.py
+++ projects/nig/backend/tasks/launch_pipeline.py
@@ -14,7 +14,7 @@ from restapi.utilities.logs import log
 from snakemake import snakemake
 
 
-@CeleryExt.task()
+@CeleryExt.task(idempotent=True, autoretry_for=(ConnectionResetError,))
 def launch_pipeline(
     self: Task,
     dataset_list: List[str],
