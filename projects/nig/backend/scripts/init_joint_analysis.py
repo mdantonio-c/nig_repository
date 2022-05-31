b024f871f31dd7677e9d7837dc200b6efaefb2ac
bchiavar <b.chiavarini@cineca.it>
2022-05-31 14:46:27 +0200

    launch joint analysis task with force param

--- projects/nig/backend/scripts/init_joint_analysis.py
+++ projects/nig/backend/scripts/init_joint_analysis.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 import sys
+from pathlib import Path
 
+import yaml
 from restapi.connectors import celery, neo4j
 from restapi.env import Env
 from restapi.utilities.logs import log
@@ -24,10 +26,19 @@ else:
 
 log.info("Sending joint analysis for datasets: {}", datasets_uuid)
 
+# check if the update param is true or false (in order to check if snakemake needs to be forced)
+source_dir = Path("/snakemake")
+config_file = source_dir.joinpath("config.yaml")
+with open(config_file) as f:
+    config = yaml.load(f)
+
+force = config["UPDATE"]["GDBI"]
+
+
 c = celery.get_instance()
 task = c.celery_app.send_task(
     "launch_joint_analysis",
-    args=(datasets_uuid,),
+    args=(datasets_uuid, "Joint_samples.smk", force),
     countdown=1,
 )
 
